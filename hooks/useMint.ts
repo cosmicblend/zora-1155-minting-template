52417f9a2b528caf2e996da5ac9e5c308bb86fc6
Chet Woodside <chet@cosmicblend.ca>
2023-11-06 07:08:51 -0800

    experiments and play

--- hooks/useMint.ts
+++ hooks/useMint.ts
@@ -7,6 +7,7 @@ import {
 import { utils, BigNumber } from 'ethers'
 
 const zora1155ToMint = '0xCfc1B8B784938A158c2729890E0348d558Af843c'
+// const zoraFixedPriceStrategyGoerli = '0x455c9D3188A3Cd94aCDE8E5Ec90cA92FC10805EA' // new goerli test
 const zoraFixedPriceStrategyGoerli = '0x10f7d171fbf4ac14a3250e9313D97b2Dfe30EcfD'
 
 const chainId = process.env.NEXT_PUBLIC_CHAIN_ID == '1' ? 1 : 5
@@ -15,6 +16,7 @@ const zoraFee = process.env.NEXT_PUBLIC_CHAIN_ID == '1' ? 777000000000000 : 1000
 export function useMint() {
     
     const recipientAddress = "0x221C863222b5Da2539b391bC6993f8e14D96e9C8" // refraction dao mainnet address
+    // const recipientAddress = "0xD1344833F3cCB6359583657BE3D8959a18AB83b2" // new recipient test
 
   const { config: prepareConfig, error: prepareError } = usePrepareContractWrite({
     address: zora1155ToMint, // address of collection to mint from
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import { useMint } from '../hooks/useMint'
-import { useCreate1155Token } from '../hooks/useCreate1155Token'
 
 const Home: NextPage = () => {
   
@@ -14,16 +13,6 @@ const Home: NextPage = () => {
     status 
   } = useMint()
 
-  // hook for creating new token
-  const {
-    newTokenWrite,
-    newTokenWaitData,
-    newTokenWaitError,
-    newTokenWaitLoading,
-    newTokenWaitSuccess,
-    newTokenWaitStatus  
-  } = useCreate1155Token()
-
   return (
     <div className="flex flex-col items-center justify-center h-screen pb-48 space-y-6">
       <button
@@ -31,13 +20,7 @@ const Home: NextPage = () => {
         onClick={() => write?.()}
         className="w-[120px] border-[1px] border-black px-3 py-1 rounded hover:bg-black hover:text-white">
         Mint
-      </button>
-      <button
-        disabled={isLoading}
-        onClick={() => newTokenWrite?.()}
-        className="w-[120px] border-[1px] border-black px-3 py-1 rounded hover:bg-black hover:text-white">
-        New Token
-      </button>      
+      </button>     
     </div>
   )
 }
